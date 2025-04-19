7494b607ae53e11e8852bf6070c8f01f08938627
vbzt <vitorcastrobuzato@gmail.com>
2025-04-18 21:55:06 -0300

    refactor: remove unused middleware from SalesModule

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -3,9 +3,7 @@ import { SalesController } from "./sales.controller";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { SalesService } from "./sales.service";
 import { AuthModule } from "src/auth/auth.module";
-import { EmployeeModule } from "src/employee/employee.module";
-import { IdCheckMiddleware } from "src/middlewares/check-id.middleware";
-import { ItemIdCheckMiddleware } from "src/middlewares/check-item-id.middleware";
+import { EmployeeModule } from "src/employee/employee.module"
 
 @Module({
   imports: [PrismaModule, AuthModule, EmployeeModule],
@@ -13,15 +11,4 @@ import { ItemIdCheckMiddleware } from "src/middlewares/check-item-id.middleware"
   controllers: [SalesController],
 })
 
-export class SalesModule implements NestModule{ 
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(IdCheckMiddleware).forRoutes({
-      path: 'sales/:id',
-      method: RequestMethod.ALL
-    })
-    consumer.apply(ItemIdCheckMiddleware).forRoutes({
-      path: 'sales/:id/items/:itemId',
-      method: RequestMethod.ALL
-    })
-  }
-}
+export class SalesModule {}
