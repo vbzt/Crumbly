4408cae5701a1b2c870253228430e85b69cc5a52
vbzt <vitorcastrobuzato@gmail.com>
2025-04-27 22:46:41 -0300

    feat: add seeder function to create a base manager if none exists

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,19 +1,23 @@
-import { Body, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateEmployeeDTO } from "./dto/create-employee.dto";
 import * as bcrypt from 'bcrypt'
 import { UpdateEmployeeDTO } from "./dto/update-employee.dto";
+import { RoleQueryDTO } from "./dto/role-query.dto";
+import { error } from "console";
 
 @Injectable()
 export class EmployeeService{
-   constructor(private readonly prisma: PrismaService) { }
+   constructor(private readonly prismaService: PrismaService) { }
 
   async createEmployee(data: CreateEmployeeDTO){
-    const existingEmployee = await this.prisma.employee.findFirst( { where: { OR: [ {  email: data.email }, { phone: data.phone } ] } })
+    const existingEmployee = await this.prismaService.employee.findFirst( { where: { OR: [ {  email: data.email }, { phone: data.phone } ] } })
     if(existingEmployee) throw new ConflictException('Employee already registered.')
+
+    if(data.role === 'MANAGER') throw new BadRequestException({ message: ["role must be one of the following values: CASHIER, STOCKER"], error: "Bad Request", statusCode: 400 })
     const salt = await bcrypt.genSalt()
     data.password = await bcrypt.hash(data.password, salt)
-    return this.prisma.employee.create( { data })
+    return this.prismaService.employee.create( { data })
   }
 
   async updateEmployee(id: number, data: UpdateEmployeeDTO){
@@ -22,26 +26,38 @@ export class EmployeeService{
       const salt = await bcrypt.genSalt()
       data.password = await bcrypt.hash(data.password, salt)
     }
-    return this.prisma.employee.update( { data, where: { id }, select: { id: true, name: true, email: true, phone: true, role: true } } )
+    return this.prismaService.employee.update( { data, where: { id }, select: { id: true, name: true, email: true, phone: true, role: true } } )
   }
 
-  async showEmployees(){ 
-    return this.prisma.employee.findMany( { select: { id: true, name: true, email: true, phone: true, role: true }})
+  async showEmployees(query: RoleQueryDTO){
+    if(query.role) return this.prismaService.employee.findMany( { where: { role: query.role }, select: { id: true, name: true, email: true, phone: true, role: true }})
+    return this.prismaService.employee.findMany( { select: { id: true, name: true, email: true, phone: true, role: true }})
   }
 
   async getEmployee(id: number){
     await this.employeeExists(id)
-    return await this.prisma.employee.findUnique( { where: {id}, select: { id: true, name: true, email: true, phone: true, role: true } } )
+    return await this.prismaService.employee.findUnique( { where: {id}, select: { id: true, name: true, email: true, phone: true, role: true } } )
   }
 
   async deleteEmployee(id:number){
     await this.employeeExists(id)
-    return await this.prisma.employee.delete( { where: {id} })
+    return await this.prismaService.employee.delete( { where: {id} })
   }
 
-  async employeeExists(id: number){ 
-    if(!(await this.prisma.employee.count({ where: { id } }))){
+ employeeExists = async (id: number) => { 
+    if(!(await this.prismaService.employee.count({ where: { id } }))){
       throw new NotFoundException('Employee do not exist')
+    }
+  }
+
+  ensuremManagerEmployee = async () => { 
+    const managerExists = await this.prismaService.employee.findFirst({ where: { role: 'MANAGER' }})
+    if(!managerExists){
+      const salt = await bcrypt.genSalt()
+      const password = await bcrypt.hash('manager123', salt)
+      
+      await this.prismaService.employee.create({ data: { name: 'manager', email:'manager@crumbly.com', phone: '1199999999', password, role: 'MANAGER' }})
+      console.log('✅ Base manager created >> manager@crumbly.com | manager123')
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { EmployeeService } from './employee/employee.service';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe( { whitelist: true, forbidNonWhitelisted: true, transform: true}))
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000)
+  const employeeService = app.get(EmployeeService)
+  await employeeService.ensuremManagerEmployee()
+
 }
-bootstrap();
+bootstrap() 
